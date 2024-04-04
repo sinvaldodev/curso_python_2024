4bd01811dbae0b363e42b411e24eba1daf957ae2
Sinvaldo Pissinatti <sinvaldo.pissinatti@ajcred.local>
2024-04-04 17:15:47 -0400

    teste 3

---
+++ Conceitos/for_while.py
@@ -0,0 +1,13 @@
+a = [1, 2, 3, 4, 5]
+b = 0
+
+# for x in a:
+#   print(x)
+#   b = b + x
+
+# print(f"somatório dos itens dentro do array é {b}")
+
+#for x in range(start, stop, step):
+
+for x in range(0, 10, 2):
+  print(x)
