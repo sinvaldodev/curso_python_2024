891e2b4f6909311443d3dff49aecb32856ea9216
Sinvaldo Pissinatti <sinvaldo.pissinatti@ajcred.local>
2024-04-04 17:47:47 -0400

    Commit dos primeiros passos do código e da adição de dois arquivos csv para leitura e análise

--- Projeto do Curso - Dashboard/main.py
+++ Projeto do Curso - Dashboard/main.py
@@ -1,4 +1,10 @@
 #o código vai aqui
 import streamlit as st
+import pandas as pd
 
-st.write("Olá, mundo")
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("./dataassets/customer reviews.csv")
+df_top100_books = pd.read_csv("./dataassets/Top-100 Trending Books.csv")
+
+df_reviews
